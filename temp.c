edabea73e349ee1c0330d20176ab2a6c3c501259
boatwrong <bryantboatright@icloud.com>
2023-05-23 22:03:21 -0500

    fixed seg fault but not printing strings???

--- temp.c
+++ temp.c
@@ -3,40 +3,77 @@
 #include<unistd.h>
 #include<stdio.h>
 
-typedef struct {
+struct node {
     char** line;
     struct node* next;
-} node;
+};
 
-node* head;
-node* tail;
+struct node* head;
 
 void init(char* str) {
-    node* tmp;
+    printf("1\n");
+    struct node* tmp = (struct node*) malloc(sizeof(struct node*));
+    printf("2\n");
     tmp->line = &str;
+    printf("3\n");
     tmp->next = NULL;
+    printf("4\n");
     head = tmp;
-    tail = tmp;
+    printf("5\n");
 }
 
 void append(char* str) {
-    node* next; 
-    next->line = &str;
-    next->next = NULL;
+    struct node* tmp = head;
 
-    node* tmp = head;
     while (tmp != NULL) {
         tmp = tmp->next;
     }
+
+    struct node* new = (struct node*) malloc(sizeof(struct node*));
+    new->line = &str;
+    new->next = NULL;
+}
+
+void printList() {
+    printf("6\n");
+    struct node* tmp = (struct node*) malloc(sizeof(struct node*));
+    printf("7\n");
+    tmp->next = head;
+
+    printf("8\n");
+    while (tmp != NULL) {
+        printf("9\n");
+        printf("10\n");
+        if (tmp->next != NULL) {
+            printf("11\n");
+            if (tmp->line != NULL) {
+                printf("%s\n", *tmp->line);
+            }
+            printf("12\n");
+            tmp = tmp->next;
+            printf("13\n");
+        }
+        else {
+            printf("14\n");
+            break;
+        }
+    }
+
 }
 
 void gitStatus();
 void appendCharStar(); 
 
 int main(int argc, char* argv[]) {
+    struct node* head = (struct node*) malloc(sizeof(struct node*));
+    printf("testing\n");
     char* a = "testing";
     char b = 'b';
+    printf("entering init()\n");
     init(a);
+    printList();
+    char* c = "hello";
+    printList();
 }
 
 void gitStatus() {
